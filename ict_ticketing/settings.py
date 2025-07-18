b701e01e236c09ff63d3379e94f7e2a0c21da79a
mcalantec <oluwatobi.akande@mcalantec.com>
2025-07-18 15:34:03 -0700

    Fix ALLOWED_HOSTS for Render

--- ict_ticketing/settings.py
+++ ict_ticketing/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-your-secret-key-her
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('redeemer-ict.onrender.com', 'localhost,127.0.0.1').split(',')
 
 # Application definition
 INSTALLED_APPS = [
